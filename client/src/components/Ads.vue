8c81d94f72184ba6bad0e2f8027b2f3e3fb0626b
Poi1802 <you@example.com>
2023-03-22 23:19:29 +0800

    made load ads in profile and multi img slider

---
+++ client/src/components/Ads.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="device flex h-44 mt-10 gap-4 relative">
+    <div
+      @click="prevImg"
+      class="left-arrow h-full absolute w-20 left-1 cursor-pointer text-gray-400 hover:text-gray-600 duration-200">
+      <i class="fa-solid fa-chevron-left text-3xl mt-20"></i>
+    </div>
+    <router-link to="#" class="device-link flex">
+      <div class="img-lists flex bg-slate-100 w-full rounded-xl">
+        <div v-for="(img, idx) in imagesArr" class="img-list">
+          <img
+            :class="{ active: activeImg === idx }"
+            class="device-img rounded-xl min-w-full min-h-full"
+            :src="`http://onlinestore.bz/assets/devices/${img}`"
+            :alt="idx" />
+        </div>
+      </div>
+    </router-link>
+    <div
+      @click="nextImg"
+      class="left-arrow h-full absolute w-20 left-40 cursor-pointer text-gray-400 hover:text-gray-600 duration-200">
+      <i class="fa-solid fa-chevron-right text-3xl mt-20 ml-12"></i>
+    </div>
+    <div class="device-info">
+      <router-link to="#">
+        <div class="device-name text-2xl text-sky-500 hover:text-red-400">
+          {{ device.name }}
+        </div>
+      </router-link>
+      <div class="device price text-xl font-bold">12 990 Р</div>
+      <div class="divece-category">Электроника</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    device: Object,
+  },
+  data: () => ({
+    activeImg: 0,
+  }),
+  computed: {
+    imagesArr() {
+      return this.device.img.split(',');
+    },
+  },
+  methods: {
+    nextImg() {
+      if (this.activeImg !== this.imagesArr.length - 1) {
+        this.activeImg++;
+      }
+    },
+    prevImg() {
+      if (this.activeImg !== 0) {
+        this.activeImg--;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.devices
+  .device
+    &-link
+      width: 230px
+      height: 176px
+
+    .img-lists
+      .img-list
+        .device-img
+          display: none
+        .active
+          display: block
+</style>
--- client/src/components/ProfileAds.vue
+++ client/src/components/ProfileAds.vue
@@ -2,41 +2,39 @@
   <h2 class="text-3xl font-bold">Мои объявления</h2>
   <hr class="mt-6" />
   <div class="devices">
-    <div class="device flex h-44 mt-10 gap-4">
-      <router-link to="#" class="device-link">
-        <div class="device-img"></div>
-      </router-link>
-      <div class="device-info">
-        <router-link to="#">
-          <div class="device-name text-2xl text-sky-500 hover:text-red-400">
-            iPhone 11
-          </div>
-        </router-link>
-        <div class="device price text-xl font-bold">12 990 Р</div>
-        <div class="divece-category">Электроника</div>
-      </div>
-    </div>
+    <Ads v-for="device in devices" :device="device" :key="device.id" />
   </div>
 </template>
 
 <script>
-export default {};
+import Ads from './Ads.vue';
+import axios from 'axios';
+import { useLoginStore } from '../stores/loginStore';
+
+export default {
+  components: {
+    Ads,
+  },
+  data: () => ({
+    devices: [],
+    login: useLoginStore(),
+  }),
+  mounted() {
+    this.getDevices();
+  },
+  methods: {
+    getDevices() {
+      axios
+        .get(`http://onlinestore.bz/server/devices?user_id=${this.login.user.id}`)
+        .then((res) => {
+          this.devices = res.data;
+        });
+    },
+  },
+};
 </script>
 
 <style lang="sass" scoped>
 hr
   border-top-width: 2px
-.devices
-  .device
-    &-link
-      width: 230px
-      height: 176px
-    &-img
-      background-image: url(http://onlinestore.bz/assets/devices/iphone.jpeg)
-      border-radius: 8px
-      background-position: center
-      background-size: 100%
-      background-repeat: no-repeat
-      width: 100%
-      height: 100%
 </style>
